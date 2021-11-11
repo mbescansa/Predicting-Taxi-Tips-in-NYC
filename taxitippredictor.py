ad6e9fa10333160d635c4c5ce7245d83feb2e9de
mbescansa <bescansamarta@gmail.com>
2021-11-11 19:51:38 +0000

    Update taxitippredictor.py

--- taxitippredictor.py
+++ taxitippredictor.py
@@ -24,7 +24,7 @@ InteractiveShell.ast_node_interactivity = "all"
 # Format
 pd.options.display.float_format = '{:,.2f}'.format
 
-final_data = pd.read_csv('app_data.csv')
+final_data = pd.read_csv('resources/app_data.csv')
 final_data.drop(['fare_amount'],axis=1, inplace=True)
 
 y = final_data['tip_amount']
